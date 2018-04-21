9a6b56bbfe2324a5ed6ce047a634948a25f1bfa3
diwangs <27280516+diwangs@users.noreply.github.com>
2018-04-21 22:32:24 +0700

    Coba lihat deklarasi2nya

--- programs/source/cat.c
+++ programs/source/cat.c
@@ -5,9 +5,9 @@ void printInt(int i);
 
 int main() {
 	char curdir, argc, res;
-	char **argv;
-	char *isifile;
-	int result, i;
+	char argv[20];
+	char isifile[10];
+	int *result, i;
 
 	// for checking
 	char checkpath;
@@ -17,34 +17,30 @@ int main() {
 	// get argc
 	interrupt(0x21, 0x22, &argc, 0, 0);
 	// get arguement(s)
-	for (i = 0; i < argc; ++i) {
-		interrupt(0x21, 0x23, i, argv[i], 0);
-		// interrupt(0x21, 0x00, "mengisi variable\r\n", 0, 0);
-	}
+	interrupt(0x21, 0x23, 0, argv, 0);
+	// interrupt(0x21, 0x00, "mengisi variable\r\n", 0, 0);
 
 	// interrupt(0x21, 0x00, "argc: ", 0 ,0);
 	// printInt(argc);
 	interrupt(0x21, 0x00, "file yang dituju: ", 0, 0);
-	interrupt(0x21, 0x00, argv[0], 0, 0);
+	interrupt(0x21, 0x00, argv, 0, 0);
 	interrupt(0x21, 0x00, "\r\n", 0, 0);
 	// interrupt(0x21, 0x00, "\r\n\n", 0, 0);
 
-	if (argc == 2 && strcmp(argv[1],"-w") == 1) {	// menuliskan isi file
+	if (argc == 2) {	// menuliskan isi file
 		// baca input user
-		interrupt(0x21, 0x00, "Masukan isi file: ", 0, 0);
-		interrupt(0x21, 0x01, isifile, 0, 0);
+		interrupt(0x21, 0x00, "Masukan isi file: \r\n", 0, 0);
+		interrupt(0x21, 0x01, isifile, 0, 0);			
 		// masukkan input user ke file yang dituju
-		interrupt(0x21, curdir << 8 | 0x05, isifile, argv[0], &result);
+		interrupt(0x21, 0xFF << 8 | 0x05, isifile, argv, result);
 
 		// checking
-		interrupt(0x21, 0x00, isifile, 0, 0);
-		interrupt(0x21, 0x00, "\r\n", 0, 0);
-		checkpath = searchFile("afile", curdir);
-		if (checkpath == 32) {
-			interrupt(0x21, 0x00, "ndak ada\r\n", 0, 0);
-		} else {
-			interrupt(0x21, 0x00, "file ada", 0, 0);
-		}
+		// checkpath = searchFile(argv, curdir);
+		// if (checkpath == 32) {
+		// 	interrupt(0x21, 0x00, "ndak ada\r\n", 0, 0);
+		// } else {
+		// 	interrupt(0x21, 0x00, "file ada", 0, 0);
+		// }
 		
 		// jika berhasil, cetak pesan berhasil
 		if (result == 0) {
@@ -53,17 +49,18 @@ int main() {
 	} else {			// menampilkan isi file
 		// asumsi file dipastikan ada di di curent directory
 		// readfile
-		interrupt(0x21, (curdir << 8) | 0x04, isifile, argv[0], &result);
+		interrupt(0x21, (curdir << 8) | 0x04, isifile, argv, result);
 		// print isi ke layar
 		if (result == 0) {
 			interrupt(0x21, 0x00, isifile, 0, 0);
+			interrupt(0x21, 0x00, "\r\n", 0, 0);			
 		}
 	}
 
 	// kembali ke shell
 	// result = 0;
 	// interrupt(0x21, 0x07, &result, 0, 0);
-	interrupt(0x21, (curdir << 8) | 0x06, "shell", 0x2000, &res);
+	interrupt(0x21, (curdir << 8) | 0x06, "shell", 0x2000, result);
 }
 
 #include "../../library/math.h"
